3fb4c67ec3222edefe9bba6e2dbf882e51a3f0af
Vadim Arkadov <vgarkadov@avito.ru>
2023-08-09 22:12:12 +0300

    Add env support

--- cmd/skill/flags.go
+++ cmd/skill/flags.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"flag"
+	"os"
 )
 
-// неэкспортированная переменная flagRunAddr содержит адрес и порт для запуска сервера
+// Не экспортированная переменная flagRunAddr содержит адрес и порт для запуска сервера
 var flagRunAddr string
 
 // parseFlags обрабатывает аргументы командной строки
 // и сохраняет их значения в соответствующих переменных
 func parseFlags() {
 	// регистрируем переменную flagRunAddr
-	// как аргумент -a со значением :8080 по умолчанию
+	// как аргумент -a со значением `:8080` по умолчанию
 	flag.StringVar(&flagRunAddr, "a", ":8080", "address and port to run server")
 	// парсим переданные серверу аргументы в зарегистрированные переменные
 	flag.Parse()
+
+	if envRunAddr := os.Getenv("RUN_ADDR"); envRunAddr != "" {
+		flagRunAddr = envRunAddr
+	}
 }
