ba648dc3701bf798a5a44d978d03b0926a6fded0
Vadim Arkadov <vgarkadov@avito.ru>
2023-08-27 14:43:42 +0300

    Add models

--- cmd/skill/main_test.go
+++ cmd/skill/main_test.go
@@ -10,15 +10,10 @@ import (
 )
 
 func TestWebhook(t *testing.T) {
-	// тип http.HandlerFunc реализует интерфейс http.Handler
-	// это поможет передать хендлер тестовому серверу
 	handler := http.HandlerFunc(webhook)
-	// запускаем тестовый сервер, будет выбран первый свободный порт
 	srv := httptest.NewServer(handler)
-	// останавливаем сервер после завершения теста
 	defer srv.Close()
 
-	// ожидаемое содержимое тела ответа при успешном запросе
 	successBody := `{
         "response": {
             "text": "Извините, я пока ничего не умею"
@@ -26,25 +21,63 @@ func TestWebhook(t *testing.T) {
         "version": "1.0"
     }`
 
-	// описываем набор данных: метод запроса, ожидаемый код ответа, ожидаемое тело
 	testCases := []struct {
+		name         string // добавляем название тестов
 		method       string
+		body         string // добавляем тело запроса в табличные тесты
 		expectedCode int
 		expectedBody string
 	}{
-		{method: http.MethodGet, expectedCode: http.StatusMethodNotAllowed, expectedBody: ""},
-		{method: http.MethodPut, expectedCode: http.StatusMethodNotAllowed, expectedBody: ""},
-		{method: http.MethodDelete, expectedCode: http.StatusMethodNotAllowed, expectedBody: ""},
-		{method: http.MethodPost, expectedCode: http.StatusOK, expectedBody: successBody},
+		{
+			name:         "method_get",
+			method:       http.MethodGet,
+			expectedCode: http.StatusMethodNotAllowed,
+			expectedBody: "",
+		},
+		{
+			name:         "method_put",
+			method:       http.MethodPut,
+			expectedCode: http.StatusMethodNotAllowed,
+			expectedBody: "",
+		},
+		{
+			name:         "method_delete",
+			method:       http.MethodDelete,
+			expectedCode: http.StatusMethodNotAllowed,
+			expectedBody: "",
+		},
+		{
+			name:         "method_post_without_body",
+			method:       http.MethodPost,
+			expectedCode: http.StatusInternalServerError,
+			expectedBody: "",
+		},
+		{
+			name:         "method_post_unsupported_type",
+			method:       http.MethodPost,
+			body:         `{"request": {"type": "idunno", "command": "do something"}, "version": "1.0"}`,
+			expectedCode: http.StatusUnprocessableEntity,
+			expectedBody: "",
+		},
+		{
+			name:         "method_post_success",
+			method:       http.MethodPost,
+			body:         `{"request": {"type": "SimpleUtterance", "command": "sudo do something"}, "version": "1.0"}`,
+			expectedCode: http.StatusOK,
+			expectedBody: successBody,
+		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.method, func(t *testing.T) {
-			// делаем запрос с помощью библиотеки resty к адресу запущенного сервера,
-			// который хранится в поле URL соответствующей структуры
 			req := resty.New().R()
 			req.Method = tc.method
 			req.URL = srv.URL
+
+			if len(tc.body) > 0 {
+				req.SetHeader("Content-Type", "application/json")
+				req.SetBody(tc.body)
+			}
 
 			resp, err := req.Send()
 			assert.NoError(t, err, "error making HTTP request")
